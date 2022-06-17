49dd50fdd094f79d7cb51bc68c2cc842fcde48ef
lRichal <xade22@gmail.com>
2022-06-17 20:53:24 -0300

    Meu Projeto SysManager

---
+++ SysManager.API.admin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SysManager.API.admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
