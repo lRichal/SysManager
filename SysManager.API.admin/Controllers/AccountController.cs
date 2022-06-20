7b981736109825aba08db8345ef1fc72f2775890
lRichal <xade22@gmail.com>
2022-06-20 20:43:56 -0300

    Criação de duas requições como: 'Create-Account' e 'Login', e criamos classes como 'UserName','email' e

---
+++ SysManager.API.admin/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using SysManager.Application.Contracts.Products.Request;
+
+namespace SysManager.API.admin.Controllers
+{   /// <summary>
+    /// controladora de contas
+    /// </summary>
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    class AccountController
+    {
+        public AccountController()
+        {
+         
+        }
+
+        [HttpPost("create-account")]
+        public async Task<string> Post(UserPostRequest request)
+        {
+            return "Requisicao efetuada com sucesso";
+        }
+
+        [HttpPost("login")]
+        public async Task<string> PostLogin(string obj)
+        {
+            return "Login efetuado com sucesso";
+        }
+    }
+}
---
+++ SysManager.Application/Contracts/Products/Request/UserPostRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SysManager.Application.Contracts.Products.Request
+{   /// <summary>
+    /// classe "Contracts" responsavel para
+    /// cadastrar um novo usuario
+    /// </summary>
+    public class UserPostRequest
+    {
+        /// <summary>
+        /// classe referente ao nome do usuario
+        /// </summary>
+      public string UserName { get; set; }
+        /// <summary>
+        /// classe referente ao email do usuario
+        /// </summary>
+      public string Email { get; set; }
+        /// <summary>
+        /// classe referente a senha do usuario
+        /// </summary>
+      public string Password { get; set; }
+
+    }
+}
